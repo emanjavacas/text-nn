0079471d47a5df33c36d43d765ddd435241bb5fd
Enrique Manjavacas <enrique.manjavacas@gmail.com>
2017-04-11 10:48:14 +0200

    added model to script

--- run_gender_length.sh
+++ run_gender_length.sh
@@ -1,22 +1,16 @@
 
+MODEL=$1
+
 # token level
 for LENGTH in $(seq 10 10 100); do
-    if [ $LENGTH -eq 10 ]; then
-	python gender.py --gpu --epochs 5 --dropout 0.3 --emb_dim 300 --load_embeddings --flavor ft --suffix wiki --level token --cache_data --min_len $LENGTH --concat
-    else
-	python gender.py --gpu --epochs 5 --dropout 0.3 --emb_dim 300 --load_embeddings --flavor ft --suffix wiki --level token --load_data --min_len $LENGTH --concat
-    fi
+	python gender.py --model $MODEL --gpu --epochs 5 --dropout 0.3 --emb_dim 300 --load_embeddings --flavor ft --suffix wiki --level token --min_len $LENGTH --concat
 done
 
 for LENGTH in $(seq 100 100 500); do
-    python gender.py --gpu --epochs 5 --dropout 0.3 --emb_dim 300 --load_embeddings --flavor ft --suffix wiki --level token --load_data --min_len $LENGTH --concat
+    python gender.py --model $MODEL --gpu --epochs 5 --dropout 0.3 --emb_dim 300 --load_embeddings --flavor ft --suffix wiki --level token --min_len $LENGTH --concat
 done
 
 # char level
 for LENGTH in $(seq 140 200 1000); do
-    if [ $LENGTH -eq 140 ]; then
-	python gender.py --gpu --epochs 5 --dropout 0.3 --emb_dim 24 --level char --cache_data --min_len $LENGTH --concat --min_freq 500
-    else
-	python gender.py --gpu --epochs 5 --dropout 0.3 --emb_dim 24 --level char --load_data --min_len $LENGTH --concat --min_freq 500
-    fi
+	python gender.py --model $MODEL --gpu --epochs 5 --dropout 0.3 --emb_dim 24 --level char --min_len $LENGTH --concat --min_freq 500
 done
