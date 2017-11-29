e50f109661bc70998730ce4cc74c082a331eec4d
Enrique Manjavacas <enrique.manjavacas@gmail.com>
2017-11-29 18:13:24 +0100

    updated loaders

--- text_nn/loaders.py
+++ text_nn/loaders.py
@@ -13,45 +13,50 @@ def identity(x):
     return x
 
 
-def segmenter(tweet, level='char'):
-    if level == 'char':
-        return list(tweet)
-    elif level == 'token':
-        return tweet.split()
-    else:
-        raise ValueError
-
-
 # Data loaders
 def load_twisty(path='/home/corpora/TwiSty/twisty-EN',
                 min_len=0,
                 level='token',
                 concat=False,
-                processor=identity):
+                processor=identity,
+                max_tweets=None):
     """
     Load twisty dataset with gender labels per tweet
     """
+    src, trg, total_tweets = [], [], 0
+
     tweets_path = os.path.join(path, 'data/tweets/en/users_id/')
     tweet_fs = set(os.listdir(tweets_path))
+
     with open(os.path.join(path, 'TwiSty-EN.json'), 'r') as fp:
         metadata = json.load(fp)
-    src, trg = [], []
+
     for user_id, user_metadata in metadata.items():
+
         if user_id + ".json" in tweet_fs:
             with open(os.path.join(tweets_path, user_id + '.json'), 'r') as fp:
                 tweets = json.load(fp)['tweets']
+
             buf = []
             for tweet_id in user_metadata['confirmed_tweet_ids']:
-                tweet = processor(tweets[str(tweet_id)]['text'])
-                tweet = segmenter(tweet, level=level)
-                buf.extend(tweet)
+
+                if max_tweets is not None and total_tweets >= max_tweets:
+                    break
+
+                buf.extend(processor(tweets[str(tweet_id)]['text']))
+                total_tweets += 1
+
                 if len(buf) > min_len:
-                    src.append(buf), trg.append([user_metadata["gender"]])
+                    src.append(buf), trg.append(user_metadata["gender"])
                     buf = []
                     continue
                 if not concat:  # discard tweet
                     buf = []
                     continue
+
+            else:               # when breaking in the inner loop
+                continue
+            break
 
     return src, trg
 
@@ -62,8 +67,9 @@ def load_dataset(src, trg, batch_size, max_size=20000, min_freq=5,
     Wrapper function for dataset with sensible, overwritable defaults
     """
     tweets_dict = Dict(pad_token='<pad>', eos_token='<eos>',
-                       bos_token='<bos>', max_size=max_size, min_freq=min_freq)
-    labels_dict = Dict(sequential=False, force_unk=False)
+                       bos_token='<bos>', max_size=max_size, min_freq=min_freq,
+                       force_unk=True)
+    labels_dict = Dict(sequential=False)
     tweets_dict.fit(src)
     labels_dict.fit(trg)
     d = {'src': tweets_dict, 'trg': labels_dict}
